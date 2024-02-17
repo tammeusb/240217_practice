b4f91824c28555459c806b47992a264813208513
tammeusb <tammeusb@gmail.com>
2024-02-17 14:33:38 -0500

    Add "style" radio buttons
    Two buttons alternate the style of the paragraph from default
    to dark via javascript HTMLElement properties

--- pages/mdn.js
+++ pages/mdn.js
@@ -1,5 +1,6 @@
 const link = document.querySelector("a");
 link.textContent = "Mozilla Developer Network";
+link.setAttribute("class", "highlight");
 link.href = "https://developer.mozilla.org"
 
 const sect = document.querySelector("section");
@@ -16,10 +17,29 @@ const text = document.createTextNode(
 const linkPara = document.querySelector("p");
 linkPara.appendChild(text);
 
-sect.appendChild(linkPara);
+//sect.appendChild(linkPara);
 
 const deleteButton = document.querySelector(".remove");
 deleteButton.addEventListener("click", () => {
     linkPara.remove();
 });
+
+const defaultStyle = document.querySelector(".default-style");
+defaultStyle.addEventListener("click", () => {
+    para.style.color = "black";
+    para.style.backgroundColor = "white";
+    para.style.padding = "10px";
+    para.style.width = "250px";
+    para.style.textAlign = "center";
+});
+
+const darkStyle = document.querySelector(".dark-style");
+darkStyle.addEventListener("click", () => {
+    para.style.color = "white";
+    para.style.backgroundColor = "black";
+    para.style.padding = "10px";
+    para.style.width = "250px";
+    para.style.textAlign = "center";
+});
+
 
